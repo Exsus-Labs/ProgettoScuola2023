abc52a4da289468361b9723885c6ccc8f58f9070
Shilze <psnstefano12@gmail.com>
2023-05-06 11:24:29 +0200

    Added user domain

---
+++ Parking/src/java/domain/User.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+
+/**
+ *
+ * @author stefa
+ */
+@Entity
+public class User implements Serializable{
+    @Id
+    private Integer id;
+    private String user;
+    private String password;
+    private String mail;
+    private String name;
+    private String surname;
+    private Date birthDate;
+
+    public User() {
+    }
+
+    public User(Integer id, String user, String password, String mail, String name, String surname, Date birthDate) {
+        this.id = id;
+        this.user = user;
+        this.password = password;
+        this.mail = mail;
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+}
